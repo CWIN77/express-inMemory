71384bb7afc92cedd4c47571c93550e8fd78bffc
CWIN77 <cwstbp0426@gmail.com>
2022-11-26 00:55:00 +0900

    redis get set 수정

--- src/routes/api/data/ctrl.js
+++ src/routes/api/data/ctrl.js
@@ -7,18 +7,24 @@ const setData = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Please add a text field");
   }
-  const goal = await Goal.create({
-    text: req.body.text
-  });
-  res.status(200).json({ data });
+  // const goal = await Goal.create({
+  //   text: req.body.text
+  // });
+
+  const redisCli = redisClient.v4;
+  const redisData = await redisCli.get('data');
+  const { data } = JSON.parse(redisData);
+  data.push({ text: req.body.text });
+  await redisCli.set('data', JSON.stringify(data));
+
+  res.status(200).json("임시값");
 });
 
 const getDatas = asyncHandler(async (req, res) => {
   // const goal = await Goal.find();
   const redisCli = redisClient.v4;
-  await redisCli.set('test', '12345678');
-  const redisData = await redisCli.get('test');
-  res.status(200).json(redisData);
+  const redisData = await redisCli.get('data');
+  res.status(200).json(JSON.parse(redisData));
 });
 
 const updateData = asyncHandler(async (req, res) => {
